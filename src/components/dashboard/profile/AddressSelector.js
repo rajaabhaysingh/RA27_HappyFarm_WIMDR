e9fcc50dd089ce02a4b11a39d7342d1de217290c
rajaabhaysingh <rajaabhaysingh233@gmail.com>
2020-06-06 17:34:36 +0530

    first commit check

--- src/components/dashboard/profile/AddressSelector.js
+++ src/components/dashboard/profile/AddressSelector.js
@@ -3,7 +3,11 @@ import React from "react";
 import Async, { makeAsyncSelect } from "react-select/async";
 
 function AddressSelector() {
-  return <div></div>;
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
 }
 
 export default AddressSelector;
